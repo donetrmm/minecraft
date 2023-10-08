35c3ff5e1a73e5a7945079c98fa8003804611d2d
donetrmm <223194@ids.upchiapas.edu.mx>
2023-10-08 00:43:29 -0600

    banner integrado

--- app/nether/page.js
+++ app/nether/page.js
@@ -1,9 +1,11 @@
 import styles from "../../styles/nether.module.css";
-import Banner from "../../components/Banner";
+import Banner from "../../components/BannerNether";
 export default function Nether(props) {
   return (
     <>
-        <Banner />
+        <Banner data={aboutNether} />
     </>
   );
 }
+
+const aboutNether = "El Nether es una dimensión en Minecraft, que se encuentra debajo del mundo normal. Es un lugar lleno de lava, fuego, criaturas hostiles y estructuras únicas."
